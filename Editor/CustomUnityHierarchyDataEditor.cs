22d4c8492e6cbb96a6fa1f53858bc1eaaa837038
IAmAGameDev <64835549+IAmAGameDev@users.noreply.github.com>
2025-04-28 17:18:00 +0100

    Update CustomUnityHierarchyDataEditor.cs

--- Editor/CustomUnityHierarchyDataEditor.cs
+++ Editor/CustomUnityHierarchyDataEditor.cs
@@ -14,50 +14,36 @@ namespace CustomUnityHierarchy
 
         public VisualTreeAsset visualTree;
 
-        private Toggle iconsMainToggle;
-        private Toggle headersMainToggle; 
-        private Toggle treeMainToggle;
-        private Toggle compactScriptIconsToggle;
-        private Toggle layersEnabledToggle;
-        private Toggle tagsEnabledToggle;
-
         private VisualElement iconsSubArea;
         private VisualElement headersSubArea;
         private VisualElement treeSubArea;
-        private VisualElement iconToDisplay1;
-        private VisualElement iconToDisplay2;
-        private VisualElement iconToDisplay3;
-        private VisualElement debugIconToDisplay1;
-        private VisualElement debugIconToDisplay2;
-        private VisualElement debugIconToDisplay3;
-
-        private ColorField headerColor;
-        private ColorField mainBranchColor;
-        private ColorField subBranchColor;
-
-        private TextField headersTextPrefix;
 
         private SerializedProperty componentIconsEnabledProperty;
         private SerializedProperty headersEnabledProperty;
         private SerializedProperty treeEnabledProperty;
 
+        /// <summary>
+        /// When the Scriptable Object is clicked this method is called.
+        /// </summary>
         private void OnEnable()
         {
-            customUnityHierarchyData = (CustomUnityHierarchyData)AssetDatabase.LoadAssetAtPath("Assets/CustomUnityHierarchyData.asset", typeof(CustomUnityHierarchyData));
+            // Gets reference to the scriptableObject.
+            customUnityHierarchyData = target as CustomUnityHierarchyData;
 
             componentIconsEnabledProperty = serializedObject.FindProperty("componentIconsEnabled");
             headersEnabledProperty = serializedObject.FindProperty("headersEnabled");
             treeEnabledProperty = serializedObject.FindProperty("treeEnabled");
 
+            // Return check if the scriptableObject is not found.
             if (customUnityHierarchyData == null)
             {
                 return;
             }
 
-            bool temp = customUnityHierarchyData.inspectorIcons.Count > 0;
+            bool inspectorIconsGreaterThanZero = customUnityHierarchyData.inspectorIcons.Count > 0;
 
             // If the list is > 0 so we have data in it, return.
-            if (temp)
+            if (inspectorIconsGreaterThanZero)
             {
                 return;
             }
@@ -68,76 +54,74 @@ namespace CustomUnityHierarchy
                 "d_cs Script Icon",
                 "d_Transform Icon",
                 "d_Camera Icon",
-                "d_console.infoicon",
-                "d_console.warnicon",
-                "d_console.erroricon"
             };
 
+            // Foreach icon to load, load it and cache it.
             foreach (string icontoLoad in iconsToLoad)
             {
                 customUnityHierarchyData.inspectorIcons.Add(new(icontoLoad, EditorGUIUtility.IconContent(icontoLoad).image));
             }
         }
 
+        /// <summary>
+        /// Called after OnEnable, this method draws the data on the inspector.
+        /// </summary>
         public override VisualElement CreateInspectorGUI()
         {
             VisualElement root = new();
 
+            // Clones the visual tree UXML.
             visualTree.CloneTree(root);
 
             // Icons
-            iconsMainToggle = root.Q<Toggle>("IconsMainToggle");
-            iconsMainToggle.RegisterCallback<ChangeEvent<bool>>(evt => OnBoolChanged(componentIconsEnabledProperty, iconsSubArea));
-            compactScriptIconsToggle = root.Q<Toggle>("CompactScriptIconsToggle");
-            compactScriptIconsToggle.RegisterCallback<ChangeEvent<bool>>(evt => RepaintHierarchy());
+            // Registers a ChangeEvent for the icons toggle.
+            root.Q<Toggle>("IconsMainToggle").RegisterCallback<ChangeEvent<bool>>(evt => OnBoolChanged(componentIconsEnabledProperty, iconsSubArea));
+            root.Q<Toggle>("CompactScriptIconsToggle").RegisterCallback<ChangeEvent<bool>>(evt => RepaintHierarchy());
 
+            // Gets reference to the sub area which will be enabled on icon toggle.
             iconsSubArea = root.Q<VisualElement>("IconsSubArea");
-            iconToDisplay1 = root.Q<VisualElement>("IconToDisplay1");
-            iconToDisplay2 = root.Q<VisualElement>("IconToDisplay2");
-            iconToDisplay3 = root.Q<VisualElement>("IconToDisplay3");
 
+            // Changes the icons to display by using the inspectorIcons it cached earlier.
+            var iconToDisplay1 = root.Q<VisualElement>("IconToDisplay1");
+            var iconToDisplay2 = root.Q<VisualElement>("IconToDisplay2");
+            var iconToDisplay3 = root.Q<VisualElement>("IconToDisplay3");
             iconToDisplay1.style.backgroundImage = new StyleBackground(customUnityHierarchyData.inspectorIcons.First(x => x.componentName == "d_cs Script Icon").componentTexture as Texture2D);
             iconToDisplay2.style.backgroundImage = new StyleBackground(customUnityHierarchyData.inspectorIcons.First(x => x.componentName == "d_Transform Icon").componentTexture as Texture2D);
             iconToDisplay3.style.backgroundImage = new StyleBackground(customUnityHierarchyData.inspectorIcons.First(x => x.componentName == "d_Camera Icon").componentTexture as Texture2D);
 
             // Headers
-            headersMainToggle = root.Q<Toggle>("HeadersMainToggle");
-            headersMainToggle.RegisterCallback<ChangeEvent<bool>>(evt => OnBoolChanged(headersEnabledProperty, headersSubArea));
+            // Registers a ChangeEvent for the headers toggle.
+            root.Q<Toggle>("HeadersMainToggle").RegisterCallback<ChangeEvent<bool>>(evt => OnBoolChanged(headersEnabledProperty, headersSubArea));
 
+            // Gets reference to the sub area which will be enabled on headers toggle.
             headersSubArea = root.Q<VisualElement>("HeadersSubArea");
 
-            headerColor = root.Q<ColorField>("HeaderColor");
-            headerColor.RegisterCallback<ChangeEvent<Color>>(evt => RepaintHierarchy());
+            // ChangeEvent for the header color.
+            root.Q<ColorField>("HeaderColor").RegisterCallback<ChangeEvent<Color>>(evt => RepaintHierarchy());
 
-            headersTextPrefix = root.Q<TextField>("HeadersTextPrefix");
-            headersTextPrefix.RegisterCallback<ChangeEvent<string>>(evt => RepaintHierarchy());
+            // ChangeEvent for the header prefix.
+            root.Q<TextField>("HeadersTextPrefix").RegisterCallback<ChangeEvent<string>>(evt => RepaintHierarchy());
 
-            // Tree
-            treeMainToggle = root.Q<Toggle>("TreeMainToggle");
-            treeMainToggle.RegisterCallback<ChangeEvent<bool>>(evt => OnBoolChanged(treeEnabledProperty, treeSubArea));
+            // Tree View
+            // Registers a ChangeEvent for the Tree View toggle.
+            root.Q<Toggle>("TreeMainToggle").RegisterCallback<ChangeEvent<bool>>(evt => OnBoolChanged(treeEnabledProperty, treeSubArea));
 
+            // Gets reference to the sub area which will be enabled on Tree View toggle.
             treeSubArea = root.Q<VisualElement>("TreeSubArea");
 
-            mainBranchColor = root.Q<ColorField>("MainBranchColor");
-            mainBranchColor.RegisterCallback<ChangeEvent<Color>>(evt => RepaintHierarchy());
-            subBranchColor = root.Q<ColorField>("SubBranchColor");
-            subBranchColor.RegisterCallback<ChangeEvent<Color>>(evt => RepaintHierarchy());
+            // ChangeEvent for the tree colors.
+            root.Q<ColorField>("MainBranchColor").RegisterCallback<ChangeEvent<Color>>(evt => RepaintHierarchy());
+            root.Q<ColorField>("SubBranchColor").RegisterCallback<ChangeEvent<Color>>(evt => RepaintHierarchy());
 
             // Layers and tags
-            layersEnabledToggle = root.Q<Toggle>("LayersMainToggle");
-            layersEnabledToggle.RegisterCallback<ChangeEvent<bool>>(evt => RepaintHierarchy());
-            tagsEnabledToggle = root.Q<Toggle>("TagsMainToggle");
-            tagsEnabledToggle.RegisterCallback<ChangeEvent<bool>>(evt => RepaintHierarchy());
+            root.Q<Toggle>("LayersMainToggle").RegisterCallback<ChangeEvent<bool>>(evt => RepaintHierarchy());
+            root.Q<Toggle>("TagsMainToggle").RegisterCallback<ChangeEvent<bool>>(evt => RepaintHierarchy());
 
-            // Debugs
-            debugIconToDisplay1 = root.Q<VisualElement>("DebugIconToDisplay1");
-            debugIconToDisplay2 = root.Q<VisualElement>("DebugIconToDisplay2");
-            debugIconToDisplay3 = root.Q<VisualElement>("DebugIconToDisplay3");
+            // Layers and tags icons
+            root.Q<VisualElement>("TextureToDisplay1").style.backgroundImage = new StyleBackground(customUnityHierarchyData.layerTexture as Texture2D);
+            root.Q<VisualElement>("TextureToDisplay2").style.backgroundImage = new StyleBackground(customUnityHierarchyData.tagTexture as Texture2D);
 
-            debugIconToDisplay1.style.backgroundImage = new StyleBackground(customUnityHierarchyData.inspectorIcons.First(x => x.componentName == "d_console.infoicon").componentTexture as Texture2D);
-            debugIconToDisplay2.style.backgroundImage = new StyleBackground(customUnityHierarchyData.inspectorIcons.First(x => x.componentName == "d_console.warnicon").componentTexture as Texture2D);
-            debugIconToDisplay3.style.backgroundImage = new StyleBackground(customUnityHierarchyData.inspectorIcons.First(x => x.componentName == "d_console.erroricon").componentTexture as Texture2D);
-
+            // Hides/Displays the sub areas based on their values.
             CheckForDisplayType(componentIconsEnabledProperty, iconsSubArea);
             CheckForDisplayType(headersEnabledProperty, headersSubArea);
             CheckForDisplayType(treeEnabledProperty, treeSubArea);
@@ -145,17 +129,30 @@ namespace CustomUnityHierarchy
             return root;
         }
 
+        /// <summary>
+        /// RepaintHierarchy method.
+        /// </summary>
         private void RepaintHierarchy()
         {
             EditorApplication.RepaintHierarchyWindow();
         }
 
+        /// <summary>
+        /// When a boolean is changed, change the subarea to hidden/shown and repaintTheHierarchy.
+        /// </summary>
+        /// <param name="prop"></param>
+        /// <param name="visualElement"></param>
         private void OnBoolChanged(SerializedProperty prop, VisualElement visualElement)
         {
             CheckForDisplayType(prop, visualElement);
             RepaintHierarchy();
         }
 
+        /// <summary>
+        /// When first loaded we check if any sub area should be hidden.
+        /// </summary>
+        /// <param name="prop"></param> Property which has a value of true/false.
+        /// <param name="visualElement"></param> visualElement to hide (none) or display (flex).
         private void CheckForDisplayType(SerializedProperty prop, VisualElement visualElement)
         {
             visualElement.style.display = prop.boolValue ? DisplayStyle.Flex : DisplayStyle.None;
